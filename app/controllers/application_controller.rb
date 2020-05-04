02b003748aec9d648de572e77c5437e78a19f798
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-04 18:38:01 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,15 @@ class ApplicationController < Sinatra::Base
   configure do
   	set :views, "app/views"
   	set :public_dir, "public"
+  	#tells the controller where to look to find the views (pages with HTML to display text in the browser) and the public directory
   end
 
   get "/" do
   	erb :index
   end
+  
+  #when a client makes a request to a server to load an application, the request is received and processed by the controller. We must create a controller action to accept the request and respond with the appropriate HTML. 
+  
+  #We did that above with a controller action that can receive and respond to a GET request to the root URL /
+  #This GET request loads the index.erb file 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,8 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 require_all 'app'
+
+
+#This file connects up all the files in our application to the appropriate gems and to each other. 
+
+#This file loads Bundler and thus all the gems in our Gemfile, as well as the MVC in our app directory. 
